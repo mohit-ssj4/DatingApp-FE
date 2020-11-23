bb47e4b5342e07a32503c0e2723849d2d3ec0c7a
mohit-ssj4 <mohitsammal1997@gmail.com>
2020-11-23 14:21:02 +0530

    dropdown functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { AccountService } from "./services/account.service";
 import { NavComponent } from "./nav/nav.component";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BsDropdownModule.forRoot()
+  ],
   declarations: [AppComponent, HelloComponent, NavComponent],
   bootstrap: [AppComponent],
   providers: [AccountService]
