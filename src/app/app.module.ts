e48c2ca240a2709e7168b2ad6bf145e61fbbfb45
mohit-ssj4 <mohitsammal1997@gmail.com>
2020-11-23 11:36:34 +0530

    Adding navigation and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from "@angular/forms";
 import { HelloComponent } from "./hello.component";
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
+import { AccountService } from "./services/account.service";
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, HelloComponent],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HelloComponent, NavComponent],
+  bootstrap: [AppComponent],
+  providers: [AccountService]
 })
 export class AppModule {}
