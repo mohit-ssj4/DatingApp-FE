4ddb349e21fdbf53be322fa01d8e3e1723ea84b7
mohit-ssj4 <mohitsammal1997@gmail.com>
2020-11-30 15:14:13 +0530

    setting up some routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,27 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { AccountService } from "./services/account.service";
 import { NavComponent } from "./nav/nav.component";
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from "./home/home.component";
+import { FooterComponent } from "./footer/footer.component";
+import { MemberListComponent } from "./members/member-list/member-list.component";
+import { ListsComponent } from "./lists/lists.component";
+import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { AppRoutingModule } from "./app-routing/app-routing.module";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, HelloComponent, NavComponent, HomeComponent, FooterComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    NavComponent,
+    HomeComponent,
+    FooterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MemberDetailComponent,
+    MessagesComponent
+  ],
   bootstrap: [AppComponent],
   providers: [AccountService]
 })
