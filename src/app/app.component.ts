49f7262f3aba46b108c475f0116bb8f53b6aad71
mohit-ssj4 <mohitsammal1997@gmail.com>
2020-11-30 14:55:34 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { HttpClient } from "@angular/common/http";
-import { Component, OnInit, VERSION } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { User } from "./models/user";
 import { AccountService } from "./services/account.service";
 
@@ -9,32 +8,14 @@ import { AccountService } from "./services/account.service";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  users: any;
-  url: string = "https://localhost:5001/api/users";
-
-  constructor(
-    private http: HttpClient,
-    private accountService: AccountService
-  ) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem("user"));
     this.accountService.setCurrentUser(user);
-  }
-
-  getUsers() {
-    this.http.get(this.url).subscribe(
-      response => {
-        this.users = response;
-      },
-      error => {
-        console.log(error);
-      }
-    );
   }
 }
